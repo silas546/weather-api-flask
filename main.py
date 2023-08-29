422aafda2ee6965dd5027777f8e73070d98fe313
Silas-Owings <silas.owings@7summitsinc.com>
2023-08-29 15:03:32 -0500

    All station data and annual data endpoints added to api

--- main.py
+++ main.py
@@ -27,5 +27,22 @@ def about(station, date):
     }
 
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "weather-data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    res = df.to_dict(orient="records")
+    return res
+
+
+@app.route("/api/v1/annual/<station>/<year>")
+def annual_data(station, year):
+    filename = "weather-data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    res = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return res
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
