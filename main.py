6085f5402c81c1ee8c9cd589b755996ba27ac12a
Silas-Owings <silas.owings@7summitsinc.com>
2023-08-29 14:42:53 -0500

    Calculated fahrenheit temp. Added a stations table to homepage

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import pandas as pd
 
 app = Flask(__name__)
 
-
+# Construct a table of stations for user reference
+stations = pd.read_csv("weather-data/stations.txt", skiprows=17)
+filtered_stations = stations[["STAID", "STANAME                                 "]]
+stations_table = filtered_stations.to_html()
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", stations=stations_table)
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -16,10 +19,11 @@ def about(station, date):
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     # Access date value from url and look up temperature / 10
     temperature = df.loc()[df['    DATE'] == date]['   TG'].squeeze() / 10
+    temperature_f = temperature * (9/5) + 32
     return {
         "station": station,
         "date": date,
-        "temperature": temperature
+        "temperature": temperature_f
     }
 
 
